8f83a100571d6fdf86b4d8c22d18e558429cdd8a
poc.hsu <poc7667@gmail.com>
2016-06-02 16:14:12 +0800

    add course api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1, defaults: {format: 'json'} do
+      resources :courses
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

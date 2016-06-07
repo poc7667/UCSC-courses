8a3aa05bd049b3a14c1923a79829e2794ac86ca9
poc.hsu <poc7667@gmail.com>
2016-06-08 07:51:33 +0800

    add deployment files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.4.1'
 
 set :application, ENV['APP_NAME']
 set :repo_url, 'git@bitbucket.org:poc7667/ucscextcourseserver.git'
